4c043d1deb737c979765dc9a893730f65d89d35c
DeiviHerrera09 <dherrera@grupoasd.com>
2024-01-29 15:01:39 -0500

    Distribución de responsabilidades

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 const express = require('express');
+const { faker } = require('@faker-js/faker');
 const app = express();
 const port = 3000;
 
-app.get('/users', (req, res) => {
-  res.json([
-    {
-      name: 'Deivi',
-      lastName: 'Herrera',
-    },
-    {
-      name: 'Dayana',
-      lastName: 'Vargas',
-    },
-  ]);
+app.get('/products', (req, res) => {
+  const products = [];
+
+  const { size } = req.query;
+
+  const limit = size || 10;
+
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.internet.userName(),
+    });
+  }
+  res.json(products);
 });
 
+// PRUEBA CON QUERYS
+
+app.get("/prueba", (req, res) =>{
+  res.send("Yo soy un filter ")
+})
+
+
 app.get('/productos/:id', (req, res) => {
-  const { id } = req.params.id;
+  const { id } = req.params;
   res.json({
     id: id,
     name: 'Arroz',
@@ -27,7 +37,7 @@ app.get('/productos/:id', (req, res) => {
 // PRÁCTICA PARAMETROS NORMALES
 
 app.get('/categories/:id', (req, res) => {
-  const { id } = req.params;
+  const id = req.params.id;
 
   if (id === '1') {
     res.json({
